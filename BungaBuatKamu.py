af9fe8786a10883cec07bb8bff4e2a704aec42b8
farismasud <masudfaris54@gmail.com>
2022-12-09 17:16:51 +0700

    update

--- BungaBuatKamu.py
+++ BungaBuatKamu.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Bunga pertama Buat ayang
 import turtle as tur
 import colorsys as cs
@@ -16,23 +15,4 @@ for j in range(25):
         tur.right(180)
         tur.circle(50,24)
 tur.hideturtle()
-=======
-#Bunga pertama Buat ayang
-import turtle as tur
-import colorsys as cs
-tur.setup(800,800)
-tur.speed(0)
-tur.width(2)
-tur.bgcolor("black")
-for j in range(25):
-    for i in range(15):
-        tur.color(cs.hls_to_rgb(i/15, j/25, 1))
-        tur.right(90)
-        tur.circle(200-j*4,90)
-        tur.left(90)
-        tur.circle(200-j*4,90)
-        tur.right(180)
-        tur.circle(50,24)
-tur.hideturtle()
->>>>>>> 562dd54b770c9d89d4337b33c267d1c580ac85cc
-tur.done
+tur.done()
--- BungaCantik.py
+++ BungaCantik.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import turtle
 
 a = turtle.Turtle()
@@ -18,25 +17,4 @@ for mi in range(10):
             a.lt(90)
         a.lt(45)
     a.pencolor(warna[mi%10])
-=======
-import turtle
-
-a = turtle.Turtle()
-b = turtle.Screen()
-
-b.bgcolor('black')
-a.pencolor('white')
-a.speed(100)
-warna = ('red', 'yellow', 'green', 'blue', 'light blue', 'purple', 'olive', 'magenta', 'beige', 'spring green')
-
-for mi in range(10):
-    for me in range(8):
-        a.speed(me+10)
-        for my in range(2):
-            a.pensize(2)
-            a.circle(80+mi*20, 90)
-            a.lt(90)
-        a.lt(45)
-    a.pencolor(warna[mi%10])
->>>>>>> 562dd54b770c9d89d4337b33c267d1c580ac85cc
 b.exitonclick()
